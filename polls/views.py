615f93d09ab5b7bd063656d46a707038b245a93f
joshua clark <joshua.clark@tee2.org>
2019-05-08 14:22:15 -0700

    added files

--- polls/views.py
+++ polls/views.py
@@ -16,12 +16,9 @@ def index(request):
 	return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-	try:
-		question = Question.objects.get(pk=question_id)
-	except Question.DoesNotExist:
-		raise Http404('Question does not exist!')
+	question = get_object_or_404(Question, pk=question_id)
 	return render(request, 'polls/detail.html', {'question': question})
-
+	
 def results(request, question_id):
 	question = get_object_or_404(Question, pk=question_id)
 	return render(request, 'polls/results.html', {'question': question})
@@ -29,7 +26,7 @@ def results(request, question_id):
 def vote(request, question_id):
 	question = get_object_or_404(Question, pk=question_id)
 	try:
-		selected_choice = question.choice_set.get(pk=question_id)
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
 	except (KeyError, Choice.DoesNotExist):
 		return render (request, 'polls/detail.html', {
 			'question': question,
