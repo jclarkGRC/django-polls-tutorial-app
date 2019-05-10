591ea806467bd2b7b1bb63f6a8bb4e02f90239bf
joshua clark <joshua.clark@tee2.org>
2019-05-09 19:38:03 -0700

    added question detail view tests

--- polls/tests.py
+++ polls/tests.py
@@ -11,6 +11,20 @@ def create_question(question_text, days):
 	time = timezone.now + datetime.timedelta(days=days)
 	return Question.objects.create(question_text=question_text, pub_date=time)
 
+class QuestionDetailViewTests(TestCase):
+
+	def test_future_question(self):
+		future_question = create_question(question_text='Future question.', days=5)
+        url = reverse('polls:detail', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+    	past_question = create_question(question_text='Past question', days=-5)
+    	url = reverse('polls:detail', args=(past_question.id,))
+    	response = self.client.get(url)
+    	self.assertContains(response, past_question.question_text)
+
 class QuestionIndexViewTests(TestCase):
 
 	def test_no_questions(self):
